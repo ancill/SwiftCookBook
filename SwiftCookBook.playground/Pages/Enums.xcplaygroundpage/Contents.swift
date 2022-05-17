4570862d2576ada3267820b3d12ab940c720526e
Ancill <42762155+ancill@users.noreply.github.com>
2022-05-17 17:49:47 +0300

    1

--- SwiftCookBook.playground/Pages/Enums.xcplaygroundpage/Contents.swift
+++ SwiftCookBook.playground/Pages/Enums.xcplaygroundpage/Contents.swift
@@ -1,34 +1,34 @@
-indirect enum ArithmeticExpression { //
+enum ArithmeticExpression {
     // указатель на конкретное значение
     case number( Int )
     // указатель на операцию сложения
-    case addition( ArithmeticExpression, ArithmeticExpression )
+    indirect case addition( ArithmeticExpression, ArithmeticExpression )
     // указатель на операцию вычитания
-    case subtraction( ArithmeticExpression, ArithmeticExpression )
+    indirect case subtraction( ArithmeticExpression, ArithmeticExpression )
     // указатель на операцию умножения
-    case multiply(ArithmeticExpression,ArithmeticExpression)
+    indirect case multiplaction( ArithmeticExpression, ArithmeticExpression )
     // указатель на операцию деления
-    case devide(ArithmeticExpression,ArithmeticExpression)
+    indirect case division( ArithmeticExpression, ArithmeticExpression )
     // указатель на операцию возведения в степень
-    case pow( ArithmeticExpression, ArithmeticExpression )
+    indirect case pow( ArithmeticExpression, ArithmeticExpression )
     // метод, проводящий операцию
-    func evaluate( _ expression: ArithmeticExpression? = nil ) -> Int {
+    func evaluate( _ expression: ArithmeticExpression? = nil ) -> Int{
         // определение типа операнда (значение или операция)
-        switch expression ?? self {
+        switch expression ?? self{
             case let .number( value ):
                 return value
             case let .addition( valueLeft, valueRight ):
-                return evaluate( valueLeft ) + evaluate( valueRight )
+                return self.evaluate( valueLeft )+self.evaluate( valueRight )
             case .subtraction( let valueLeft, let valueRight ):
-                return evaluate( valueLeft ) - evaluate( valueRight )
-            case .multiply(let valueLeft, let valueRight ):
-                return evaluate(valueLeft) * evaluate(valueRight)
-            case .devide(let valueLeft, let valueRight):
-                return evaluate(valueLeft) / evaluate(valueRight)
+                return self.evaluate( valueLeft )-self.evaluate( valueRight )
+            case .multiplaction( let valueLeft, let valueRight ):
+                return self.evaluate( valueLeft )*self.evaluate( valueRight )
+            case .division( let valueLeft, let valueRight ):
+                return self.evaluate( valueLeft )/self.evaluate( valueRight )
             case .pow( let valueLeft, let valueRight ):
                 if self.evaluate( valueRight ) == 0 {
                     return 1
-                } else {
+                }else{
                     var result = self.evaluate( valueLeft )
                     for _ in 1..<self.evaluate( valueRight ){
                         result *= evaluate( valueLeft )
@@ -38,5 +38,65 @@ indirect enum ArithmeticExpression { //
         }
     }
 }
-let hardExpr = ArithmeticExpression.addition( .number(20),.subtraction( .number(10), .number(34) ) )
-let hardExprMulti = ArithmeticExpression.addition( .number(20),.multiply( .number(10), .number(34) ) )
+var hardExpr = ArithmeticExpression.pow( .number(3), .number(3) )
+hardExpr.evaluate() // 27
+hardExpr = ArithmeticExpression.multiplaction( .number(3), .number(3) )
+hardExpr.evaluate() // 9
+
+
+
+
+
+
+//1-3
+enum Chessman {
+    enum Color: String {
+        case white = "white"
+        case black = "black"
+    }
+    case king(color: Color)
+    case queen(color: Color)
+    case bishop(color: Color)
+    case knight(color: Color)
+    case rook(color: Color)
+    case pawn(color: Color)
+}
+//4
+var whiteKing: Chessman = .king(color: .white)
+var blackKing: Chessman = .king(color: .black)
+var whiteBishop: Chessman = .bishop(color: .white)
+
+//5
+//Способ 1
+//В операторе switch вы можете указать конкретное значение элемента перечисления и связанного с ним параметра
+//Примечание: В данном примере  switch-case содержит только 4 варианта (+ блок default). Все остальные варианты вы можете добавить по аналогии.
+switch whiteKing {
+    case .king(color: .white):
+        print("Белый король")
+        break
+    case .king(color: .black):
+        print("Черный король")
+    case .bishop(color: .white):
+        print("Белый король")
+    case .bishop(color: .black):
+        print("Черный король")
+    default:
+        break
+}
+
+//Способ 2
+// Использование связанного параметра
+// Связанный параметр (с ним мы знакомимлись при изучении оператора switch) указывает вместе с оператором let или var
+switch whiteKing {
+    case .king(let a):
+        if a == .white {
+            print("Белый король")
+        }else{
+            print("Черный король")
+        }
+    default:
+        break
+}
+
+//6
+var color: Chessman.Color = .black
