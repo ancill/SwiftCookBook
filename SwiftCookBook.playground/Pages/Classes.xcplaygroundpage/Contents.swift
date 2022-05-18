5e1e3299cc1996c12e8265f6bf50dbe15c6c56e6
Ancill <42762155+ancill@users.noreply.github.com>
2022-05-18 16:09:14 +0300

    1

--- SwiftCookBook.playground/Pages/Classes.xcplaygroundpage/Contents.swift
+++ SwiftCookBook.playground/Pages/Classes.xcplaygroundpage/Contents.swift
@@ -1,3 +1,25 @@
-import Cocoa
+// Отличие -- значение класса передается по ссылке, а структуры — копированием.
 
-var greeting = "Hello, playground"
+class Link {
+    var property: Int
+    init(_ property:Int){
+        self.property = property
+    }
+}
+struct Copy {
+    var property: Int
+}
+// экземпляры классов передаются по ссылке
+var value = Link(10)
+var valueByLink = value
+// изменим значение второго параметра
+valueByLink.property = 12
+// значение первого так же изменилось
+value.property // 12
+// экземпляры структур передаются копированием
+var structValue = Copy(property: 15)
+var anotherSctructValue = structValue
+// изменив значение копии
+anotherSctructValue.property = 12
+// исходное значение не меняется
+structValue.property // 15
--- SwiftCookBook.playground/Pages/Structs.xcplaygroundpage/Contents.swift
+++ SwiftCookBook.playground/Pages/Structs.xcplaygroundpage/Contents.swift
@@ -2,68 +2,66 @@ import Foundation
 
 struct Point {
     var coodinates: (x: Int, y: Int)
-    func getDistance( _ point: Point) -> Double{
+    func getDistance(_ point: Point) -> Double {
         return sqrt(
-            pow(Double( point.coodinates.x - self.coodinates.x ), 2) +
-            pow(Double( point.coodinates.y - self.coodinates.y ), 2)
+            pow(Double(point.coodinates.x - self.coodinates.x), 2) +
+                pow(Double(point.coodinates.y - self.coodinates.y), 2)
         )
     }
 }
 
-var p1 = Point(coodinates: (10,20))
-var p2 = Point(coodinates: (15,22))
+sqrt(29)
+pow(3, 2)
+var p1 = Point(coodinates: (10, 20))
+var p2 = Point(coodinates: (15, 22))
 p1.getDistance(p2)
 
-//Ответ
-//1
-enum Color: String {
-    case white = "white"
-    case black = "black"
+// ------------------------------------------------------------
+enum Type {
+    case king
+    case queen
+    case bishop
+    case knight
+    case rook
+    case pawn
 }
-//2
-enum Type: String {
-    case king = "king"
-    case queen = "queen"
-    case bishop = "bishop"
-    case knight = "knight"
-    case rook = "rook"
-    case pawn = "pawn"
+
+enum Color {
+    case white
+    case black
 }
-//3
-var color: Color = .white
-var figure: Type = .king
-//4
+
 struct Chessman {
     var color: Color
     var type: Type
-    var coordinates: (Character, UInt)?
+    var coordinates: (char: Character, num: UInt)?
     var symbol: Character
-    //инициализатор
+
     init(color: Color, type: Type) {
         self.color = color
         self.type = type
         self.coordinates = nil
-        // символ-заполнитель
         self.symbol = "?"
     }
-    //инициализатор
-    init(color: Color, type: Type, coordinates: (Character, UInt), symbol: Character) {
+
+    init(color: Color, type: Type, coordinates: (char: Character, num: UInt)?, symbol: Character) {
         self.color = color
         self.type = type
         self.coordinates = coordinates
         self.symbol = symbol
     }
-    //установка координат
+
     mutating func setCoordinates(char: Character, num: UInt) {
         self.coordinates = (char, num)
     }
-    //убрать фигуру с поля
-    mutating func kill(){
+
+    mutating func kill() {
         self.coordinates = nil
     }
 }
-//5
+
+// 5
 var whiteKing = Chessman(color: .white, type: .king)
 whiteKing.setCoordinates(char: "C", num: 3)
 whiteKing.kill()
-var blackKing = Chessman(color: .black, type: .king, coordinates: ("A",2), symbol: "\u{265A}")
+var blackKing = Chessman(color: .black, type: .king, coordinates: ("A", 2), symbol: "\u{265A}")
