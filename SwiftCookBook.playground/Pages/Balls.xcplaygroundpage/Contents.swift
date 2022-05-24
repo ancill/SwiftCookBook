509a9329aa43bdd06fde1aa205be1c71c4c7e4ab
Ancill <42762155+ancill@users.noreply.github.com>
2022-05-24 19:26:13 +0300

    1

---
+++ SwiftCookBook.playground/Pages/Balls.xcplaygroundpage/Contents.swift
@@ -0,0 +1,4 @@
+import PlaygroundSupport
+import UIKit
+
+var greeting = "Hello, playground"
---
+++ SwiftCookBook.playground/Pages/Balls.xcplaygroundpage/Sources/Ball.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+protocol BallProtocol {
+    init(color: UIColor, radius: Int, coordinates: (x: Int, y: Int))
+}
+
+public class Ball: UIView, BallProtocol {
+    required public init(color: UIColor, radius: Int, coordinates: (x:Int, y: Int)) {
+        // создание графического прямоугольника
+        super.init(frame:
+            CGRect(x: coordinates.x,
+                   y: coordinates.y,
+                   width: radius * 2,
+                   height: radius * 2))
+        // скругление углов
+        self.layer.cornerRadius = self.bounds.width / 2.0
+        // изменение цвета фона
+        self.backgroundColor = color
+    }
+
+}
