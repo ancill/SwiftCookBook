b8eb60692a6121825242f3da15d896ecea10287e
Ancill <42762155+ancill@users.noreply.github.com>
2022-05-29 09:27:57 +0300

    1

---
+++ Apps/RGBApp/RGBApp/RGBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBAppApp.swift
+//  RGBApp
+//
+//  Created by 19072382 on 29.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
