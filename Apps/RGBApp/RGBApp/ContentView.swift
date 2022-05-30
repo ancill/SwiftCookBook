c5df0021db88f3c1a132e12fb50df024f94d14e2
Ancill <42762155+ancill@users.noreply.github.com>
2022-05-30 08:39:11 +0300

    1

--- Apps/RGBApp/RGBApp/ContentView.swift
+++ Apps/RGBApp/RGBApp/ContentView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var redComponent: Double = 0.5
+    @State var greenComponent: Double = 0.5
+    @State var blueComponent: Double = 0.5
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Slider(value: $redComponent)
+                .padding(.vertical, 50.0)
+            Slider(value: $greenComponent)
+                .padding(.bottom, 50.0)
+            Slider(value: $blueComponent)
+                .padding(.bottom, 20.0)
+            Color(red: redComponent, green: greenComponent, blue: blueComponent)
+        }
+        .padding(.all, 10.0)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
