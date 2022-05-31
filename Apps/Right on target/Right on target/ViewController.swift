bacde06f9608e0726080d86de81937fd3692ee0a
Ancill <42762155+ancill@users.noreply.github.com>
2022-05-31 09:39:23 +0300

    Added new project

---
+++ Apps/Right on target/Right on target/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Right on target
+//
+//  Created by 19072382 on 30.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
