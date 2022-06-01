67694b6032cded3f23a6604bc70fa262edf6b9ec
Ancill <42762155+ancill@users.noreply.github.com>
2022-06-01 23:51:19 +0300

    Added segue to second view

---
+++ Apps/Right on target/Right on target/SecondViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SecondViewController.swift
+//  Right on target
+//
+//  Created by 19072382 on 01.06.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func loadView() {
+        super.loadView()
+        print("loadView SecondViewController")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad SecondViewController")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear SecondViewController")
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear SecondViewController")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear SecondViewController")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear SecondViewController")
+    }
+}
--- Apps/Right on target/Right on target/ViewController.swift
+++ Apps/Right on target/Right on target/ViewController.swift
@@ -14,6 +14,19 @@ class ViewController: UIViewController {
     var round: Int = 1
     var points: Int = 0
 
+    //  Метод loadView выполняется первым в жизненном цикле. Как и viewDidLoad, он вызывается лишь один раз за все время жизни сцены.
+    override func loadView() {
+        super.loadView()
+        // Создаем метку для вывода номера версии
+        let versionLabel = UILabel(frame: CGRect(x: 20, y: 20, width: 300, height: 20))
+        // изменяем текст метки
+        versionLabel.text = "Версия 1.1 "
+        // добавляем метку в родительский view
+        self.view.addSubview(versionLabel)
+        print("loadView")
+    }
+
+    //  метод viewDidLoad() вызывается только один раз за всю жизнь сцены. После loadView когда тот загрузил сцену
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
@@ -21,6 +34,26 @@ class ViewController: UIViewController {
         self.number = Int.random(in: 1 ... 50)
         // устанавливаем загаданное число в метку
         self.label.text = String(self.number)
+    }
+
+    /*:
+     Метод viewDidAppear вызывается после того, как графические элементы сцены добавлены в иерархию view. В данном методе вы можете произвести действия, которые должны быть выполнены уже после отображения элементов на экране (например, запустить анимацию на сцене или синхронизировать дан- ные с сервером).
+     */
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidApear")
+    }
+
+    // Методы viewWillDisappear и viewDidDisappear похожи на viewWillAppear и viewDidAppear c той лишь разницей,
+    // что они вызываются до и после удаления элементов сцены из иерархии view.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
     }
 
     @IBAction func checkNumber() {
