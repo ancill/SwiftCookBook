12bad7db976863c71327139888167b4a04e6770d
Ancill <42762155+ancill@users.noreply.github.com>
2022-06-23 13:47:43 +0300

    1

---
+++ Apps/NavigationApp/NavigationApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NavigationApp
+//
+//  Created by 19072382 on 22.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // link to storyboard 
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
