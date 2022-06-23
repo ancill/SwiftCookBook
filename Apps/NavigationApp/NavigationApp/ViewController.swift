9b9d20d5069c9039493f75f42f470a568ed59ca5
Ancill <42762155+ancill@users.noreply.github.com>
2022-06-23 14:14:38 +0300

    navigation example

--- Apps/NavigationApp/NavigationApp/ViewController.swift
+++ Apps/NavigationApp/NavigationApp/ViewController.swift
@@ -9,14 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // link to storyboard 
+    // link to storyboard where viewController exist
+    let storyboardInstance = UIStoryboard(name: "Main", bundle: nil)
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationItem.backButtonTitle = "Назад"
     }
+    
+    @IBAction func toGreenScene(_ sender: UIButton) {
+        // получаем ссылку на следующий контроллер
+        // в данном случае следующий - это зеленый
+        let nextViewController = storyboardInstance.instantiateViewController(withIdentifier: "greenViewController")
+        // обращаемся к Navigation Controller
+        // и вызываем метод перехода к новому контроллеру
+        self.navigationController?.pushViewController(nextViewController, animated: true)
+    }
+    
+    
 
-
+    @IBAction func toOrangeScene(_ sender: UIButton) {
+        let nextViewController = storyboardInstance.instantiateViewController(withIdentifier: "orangeViewController")
+        self.navigationController?.pushViewController(nextViewController, animated: true)
+    }
+    
+    @IBAction func toRootScene(_ sender: UIButton) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @IBAction func toPreviousScene(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
 }
 
