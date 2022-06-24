a2b3e8023e6d02e8df1d5509befdf267fcbba36b
Ancill <42762155+ancill@users.noreply.github.com>
2022-06-24 12:13:28 +0300

    Added New Project

---
+++ Apps/TransferApp/TransferApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransferApp
+//
+//  Created by 19072382 on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
