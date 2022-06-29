d2f9752f2b8486c840b23433a6df07e05b06889b
Ancill <42762155+ancill@users.noreply.github.com>
2022-06-29 23:09:08 +0300

    1

---
+++ Apps/SimpleGame/SimpleGame/SimpleGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleGameApp.swift
+//  SimpleGame
+//
+//  Created by 19072382 on 29.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
