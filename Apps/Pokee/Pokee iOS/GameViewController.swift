25de517619f003798eebbe70c071f00ce3ad17d9
Ancill <42762155+ancill@users.noreply.github.com>
2022-07-01 09:49:01 +0300

    1

---
+++ Apps/Pokee/Pokee iOS/GameViewController.swift
@@ -0,0 +1,43 @@
+//
+//  GameViewController.swift
+//  Pokee iOS
+//
+//  Created by 19072382 on 30.06.2022.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
