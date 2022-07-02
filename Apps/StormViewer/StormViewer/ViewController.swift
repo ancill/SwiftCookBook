12f511cbab93c43383aa2e61d1bdc6911e0c5cc6
Ancill <42762155+ancill@users.noreply.github.com>
2022-07-02 22:26:44 +0300

    StormViewer done

--- Apps/StormViewer/StormViewer/ViewController.swift
+++ Apps/StormViewer/StormViewer/ViewController.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    var pictures = [String]()
+    var pictures = [String]() {
+        didSet  {
+            pictures.sort  { (lhs: String, rhs: String) -> Bool in
+                return lhs > rhs
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -32,5 +40,14 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text  = pictures[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            vc.totalPictures = pictures.count
+            vc.selectedPictureNumber = indexPath.row
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 }
