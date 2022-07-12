27bef3ac090ef63bfa9cf97ee5423a079e4da564
Ancill <42762155+ancill@users.noreply.github.com>
2022-07-12 22:49:03 +0300

    1

--- Apps/Gloopdrop/Gloopdrop/GameScene.swift
+++ Apps/Gloopdrop/Gloopdrop/GameScene.swift
@@ -1,5 +1,22 @@
 import GameplayKit
 import SpriteKit
 class GameScene: SKScene {
-    override func didMove(to view: SKView) {}
+    override func didMove(to view: SKView) {
+        // Set up background
+        let background = SKSpriteNode(imageNamed: "background_1")
+        background.anchorPoint = CGPoint(x: 0, y: 0)
+        background.position = CGPoint(x: 0, y: 0)
+        addChild(background)
+
+        // Set up foreground
+        let foreground = SKSpriteNode(imageNamed: "foreground_1")
+        foreground.anchorPoint = CGPoint(x: 0, y: 0)
+        foreground.position = CGPoint(x: 0, y: 0)
+        addChild(foreground)
+        
+        // Set up player
+        let player = Player()
+        player.position = CGPoint(x: size.width/2, y: foreground.frame.maxY)
+        addChild(player)
+    }
 }
--- Apps/Gloopdrop/Gloopdrop/GameViewController.swift
+++ Apps/Gloopdrop/Gloopdrop/GameViewController.swift
@@ -4,6 +4,28 @@ import UIKit
 class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+        // Create the view
+        if let view = view as! SKView? { // Create the scene
+            // let scene = GameScene(size: view.bounds.size)
+            let scene = GameScene(size: CGSize(width: 1336, height: 1024))
+            print("scene.size: \(scene.size)")
+            // Set the scale mode to scale to fill the view window
+            scene.scaleMode = .aspectFill
+            // Set the background color
+            scene.backgroundColor = UIColor(
+                red: 105/255,
+                green: 157/255,
+                blue: 181/255,
+                alpha: 1.0)
+            // Present the scene
+            view.presentScene(scene)
+            // Set the view options
+            view.ignoresSiblingOrder = false
+            view.showsPhysics = false
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
     }
 
     override var shouldAutorotate: Bool {
