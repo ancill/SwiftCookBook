bc3eabf5a3987fa971c49e0df478713a1c85690a
Ancill <42762155+ancill@users.noreply.github.com>
2022-07-14 20:30:35 +0300

    1

--- Apps/TodoApp/TodoApp/TaskTypeCell.swift
+++ Apps/TodoApp/TodoApp/TaskTypeCell.swift
@@ -10,17 +10,6 @@ import UIKit
 class TaskTypeCell: UITableViewCell {
     @IBOutlet var typeTitle: UILabel!
     @IBOutlet var typeDescription: UILabel!
-    // 1. кортеж, описывающий тип задачи
-    typealias TypeCellDescription = (type: TaskPriority, title: String, description: String)
-    // 2. коллекция доступных типов задач с их описанием
-    private var taskTypesInformation: [TypeCellDescription] = [
-        (type: .important, title: "Важная",
-         description: "Такой тип задач является наиболее приоритетным для выполнения. Все важные задачи выводятся в самом верху списка задач"),
-        (type: .normal, title: "Текущая", description: "Задача с обычным приоритетом")
-    ]
-    // 3. выбранный приоритет
-    var selectedType: TaskPriority = .normal
-
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -32,4 +21,6 @@ class TaskTypeCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+
 }
--- Apps/TodoApp/TodoApp/TaskTypeController.swift
+++ Apps/TodoApp/TodoApp/TaskTypeController.swift
@@ -1,9 +1,22 @@
 import UIKit
 
 class TaskTypeController: UITableViewController {
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    // 1. кортеж, описывающий тип задачи
+    typealias TypeCellDescription = (type: TaskPriority, title: String, description: String)
+    // 2. коллекция доступных типов задач с их описанием
+    private var taskTypesInformation: [TypeCellDescription] = [
+        (type: .important, title: "Важная", description: "Такой тип задач является наиболее приоритетным для выполнения. Все важные задачи выводятся в самом верху списка задач"),
+        (type: .normal, title: "Текущая", description: "Задача с обычным приоритетом")
+    ]
+    // 3. выбранный приоритет
+    var selectedType: TaskPriority = .normal
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskTypesInformation.count
     }
 
     override func viewDidLoad() { super.viewDidLoad()
@@ -11,5 +24,22 @@ class TaskTypeController: UITableViewController {
         let cellTypeNib = UINib(nibName: "TaskTypeCell", bundle: nil)
         // 2. регистрация кастомной ячейки в табличном представлении
         tableView.register(cellTypeNib, forCellReuseIdentifier: "TaskTypeCell")
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 1. получение переиспользуемой кастомной ячейки по ее идентификатору
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTypeCell", for: indexPath) as! TaskTypeCell
+        // 2. получаем текущий элемент, информация о котором должна быть выведена в строке
+        let typeDescription = taskTypesInformation[indexPath.row]
+        // 3. заполняем ячейку данными
+        cell.typeTitle.text = typeDescription.title
+        cell.typeDescription.text = typeDescription.description
+        // 4. если тип является выбранным, то отмечаем галочкой
+        if selectedType == typeDescription.type {
+            cell.accessoryType = .checkmark // в ином случае снимаем отметку
+        } else {
+            cell.accessoryType = .none
+        }
+        return cell
     }
 }
