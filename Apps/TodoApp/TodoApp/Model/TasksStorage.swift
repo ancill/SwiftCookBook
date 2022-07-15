95a793d90be5d75a037179509c74c8af55274dad
Ancill <42762155+ancill@users.noreply.github.com>
2022-07-15 19:57:00 +0300

    fixed height of task type cell

--- Apps/TodoApp/TodoApp/Model/TasksStorage.swift
+++ Apps/TodoApp/TodoApp/Model/TasksStorage.swift
@@ -1,3 +1,5 @@
+import UIKit
+
 protocol TasksStorageProtocol {
     func loadTasks() -> [TaskProtocol]
     func saveTasks(_ tasks: [TaskProtocol])
@@ -5,20 +7,49 @@ protocol TasksStorageProtocol {
 
 // Сущность "Хранилище задач"
 class TasksStorage: TasksStorageProtocol {
-    func loadTasks() -> [TaskProtocol] {
-        // временная реализация, возвращающая тестовую коллекцию задач
-        let testTasks: [TaskProtocol] = [
-            Task(title: "Купить хлеб", type: .normal, status: .planned),
-            Task(title: "Помыть кота", type: .important, status: .planned),
-            Task(title: "Отдать долг Арнольду", type: .important, status: .completed),
-            Task(title: "Купить новый пылесос", type: .normal, status: .completed),
-            Task(title: "Подарить цветы супруге", type: .important, status: .planned),
-            Task(title: "Позвонить родителям", type: .important, status: .completed),
-            Task(title: "Пригласить на вечеринку Дольфа, Джеки, Леонардо, Уилла и Брюса", type: .important, status: .planned)
-        ]
-
-        return testTasks
+    // Ссылка на хранилище
+    private var storage = UserDefaults.standard
+    // Ключ, по которому будет происходить сохранение и загрузка хранилища из User Defaults
+    var storageKey: String = "tasks"
+    // Перечисление с ключами для записи в User Defaults
+    private enum TaskKey: String {
+        case title
+        case type
+        case status
     }
+
+    func loadTasks() -> [TaskProtocol] {
+        var resultTasks: [TaskProtocol] = []
+        let tasksFromStorage = storage.array(forKey: storageKey) as?
+            [[String: String]] ?? []
+        for task in tasksFromStorage {
+            guard let title = task[TaskKey.title.rawValue],
+                  let typeRaw = task[TaskKey.type.rawValue],
+                  let statusRaw = task[TaskKey.status.rawValue]
+            else {
+                continue
+            }
+            let type: TaskPriority = typeRaw == "important" ? .important : .normal
+            let status: TaskStatus = statusRaw == "planned" ? .planned : .completed
+            resultTasks.append(Task(title: title, type: type, status: status))
+        }
+        return resultTasks
+    }
+
+//    func loadTasks() -> [TaskProtocol] {
+//        // временная реализация, возвращающая тестовую коллекцию задач
+//        let testTasks: [TaskProtocol] = [
+//            Task(title: "Купить хлеб", type: .normal, status: .planned),
+//            Task(title: "Помыть кота", type: .important, status: .planned),
+//            Task(title: "Отдать долг Арнольду", type: .important, status: .completed),
+//            Task(title: "Купить новый пылесос", type: .normal, status: .completed),
+//            Task(title: "Подарить цветы супруге", type: .important, status: .planned),
+//            Task(title: "Позвонить родителям", type: .important, status: .completed),
+//            Task(title: "Пригласить на вечеринку Дольфа, Джеки, Леонардо, Уилла и Брюса", type: .important, status: .planned)
+//        ]
+//
+//        return testTasks
+//    }
 
     func saveTasks(_ tasks: [TaskProtocol]) {}
 }
